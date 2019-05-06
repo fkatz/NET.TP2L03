54143afbe4917543199199244fadf3b36327f4f6
utnnet2019@outlook.com <utnnet2019@outlook.com>
2019-05-06 18:13:36 -0300

    .

---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic : BusinessLogic
+    {
+        Data.Database.UsuarioAdapter UsuarioData { get; set; }
+        UsuarioLogic()
+        {
+            UsuarioData = new Data.Database.UsuarioAdapter();
+        }
+        public Usuario GetOne(int ID)
+        {
+            return UsuarioData.GetOne(ID);
+        }
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+        public void Save(Usuario usuario)
+        {
+            UsuarioData.Save(usuario);
+        }
+        public void Delete(int ID)
+        {
+            UsuarioData.Delete(ID);
+        }
+    }
+}
