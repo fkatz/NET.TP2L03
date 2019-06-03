f6b7c1739a7beedffd3fc32faac4e57ab6d1c071
utnnet2019@outlook.com <utnnet2019@outlook.com>
2019-06-03 18:14:37 -0300

    .

--- UI.Desktop/Program.cs
+++ UI.Desktop/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UI.Desktop
@@ -13,7 +16,7 @@ namespace UI.Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Usuarios());
         }
     }
 }
---
+++ UI.Desktop/Usuarios.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+            dgvUsuarios.AutoGenerateColumns = false;
+        }
+
+        private void mnuSalir_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            ul.GetAll();
+            this.dgvUsuarios.DataSource = ul.GetAll();
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
